105c509740eef5e242b02e8b6917b57d2598f1ce
Michael Coutanche <35501729+mcoutanche@users.noreply.github.com>
2018-03-27 15:15:45 +0100

    Updated

--- convert-windows-vm-to-hub/convert-windows-vm-to-hub.ps1
+++ convert-windows-vm-to-hub/convert-windows-vm-to-hub.ps1
@@ -1,9 +1,9 @@
 # https://docs.microsoft.com/en-us/azure/virtual-machines/windows/hybrid-use-benefit-licensing
  
 # Convert to using Azure Hybrid Benefit for Windows Server
-$vm = Get-AzureRmVM -ResourceGroup "SQL-TEST-001" -Name "SQL01"
+$vm = Get-AzureRmVM -ResourceGroup "rg-name" -Name "vm-name"
 $vm.LicenseType = "Windows_Server"
-Update-AzureRmVM -ResourceGroupName "SQL-TEST-001" -VM $vm
+Update-AzureRmVM -ResourceGroupName "rg-name" -VM $vm
 
 # Convert back to pay as you go
 $vm = Get-AzureRmVM -ResourceGroup "rg-name" -Name "vm-name"
@@ -11,7 +11,7 @@ $vm.LicenseType = "None"
 Update-AzureRmVM -ResourceGroupName rg-name -VM $vm
 
 # Verify your VM is utilizing the licensing benefit
-Get-AzureRmVM -ResourceGroup "SQL-TEST-001" -Name "SQL01"
+Get-AzureRmVM -ResourceGroup "rg-name" -Name "vm-name"
 
 # List all Azure Hybrid Benefit for Windows Server VMs in a subscription
 $vms = Get-AzureRMVM 
